247cd2f41aacb50b89ad57ad2af0c5e115338621
Mobeen88 <mobeenali1238@gmail.com>
2025-10-30 14:41:32 +0000

    Added Register and improved Search page

--- routes/main.js
+++ routes/main.js
@@ -8,15 +8,29 @@ var shopData = {shopname: "The Thirsty Student",
 
 //Handle the main routes
 router.get("/", (req, res) => {
-    res.render("index.ejs", shopData)
+    res.render("index.ejs", shopData);
 });
 
 router.get("/about", (req, res) => {
-    res.render("about.ejs", shopData)
+    res.render("about.ejs", shopData);
 });
 
 router.get("/search", (req, res) => {
-    res.render("search.ejs", shopData)
+    res.render("search.ejs", shopData);
+});
+
+//Search in the database
+router.get("/search_result", function (req, res) {
+    res.send("You Search For: " + req.query.search_text + " in " + req.query.catergory);
+});
+
+router.get("/register", (req, res) => {
+    res.render("register.ejs", shopData);
+});
+
+router.post("/registered", (req, res) => {
+    res.send("Hello " + req.body.first + " " + req.body.last + " you have now registered!"
+         + "We will send an email to you at: " + req.body.email);
 });
 
 //Export the router object so index.js can access it
